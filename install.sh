bfae426abb344a9588b700440f9f73712d791647
Exocen <Exocen@users.noreply.github.com>
2016-06-10 09:41:08 +0200

    Update install.sh
    add free repo to fedora

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ function detectOS {
         fi
     elif [ -f /etc/redhat-release ]; then
         WOS="Fedora"
+        sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
     elif [ -f /etc/centos-release ]; then
         WOS="CentOS"
     elif [ -f /etc/debian_version ]; then
