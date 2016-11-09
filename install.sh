c7eb3fd730d9f4d467d6f580b980598ca4d6c1b4
Exocen <Exocen@users.noreply.github.com>
2016-11-09 15:16:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -88,13 +88,13 @@ function make {
     if [ "$1" = "c"]
     then
     {
-    sudo "[google-chrome]
-    name=google-chrome - \$basearch
-    baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
-    enabled=1
-    gpgcheck=1
-    gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub" >> /etc/yum.repos.d/google-chrome.repo
-    ins google-chrome-stable
+    wget https://dl-ssl.google.com/linux/linux_signing_key.pub
+
+    rpm --import linux_signing_key.pub
+
+    sh -c 'echo "[google-chrome]
+    name=Google Chrome 32-bit
+    baseurl=http://dl.google.com/linux/chrome/rpm/stable/i386" >> /etc/yum.repos.d/google-chrome.repo'
     }
     else
         {
