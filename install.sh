65a875294eb93b1513f86e9f9d3b43d9ebeafa4b
Exocen <Exocen@users.noreply.github.com>
2016-11-09 15:19:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,9 +90,9 @@ function make {
     {
     wget https://dl-ssl.google.com/linux/linux_signing_key.pub
 
-    rpm --import linux_signing_key.pub
+    sudo rpm --import linux_signing_key.pub
 
-    sh -c 'echo "[google-chrome]
+    sudo sh -c 'echo "[google-chrome]
     name=Google Chrome 32-bit
     baseurl=http://dl.google.com/linux/chrome/rpm/stable/i386" >> /etc/yum.repos.d/google-chrome.repo'
     }
