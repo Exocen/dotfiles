a731034f5bb4e5bb121c7f6eaacd4557da38cb47
Exocen <Exocen@users.noreply.github.com>
2017-05-20 19:54:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ function make {
   then
     {
       home_ln .i3
-      ins clementine tig nethogs nitrogen numlockx mcomix blueman pulseaudio-bluetooth #bluetooth
+      ins clementine tig nethogs nitrogen numlockx mcomix ttf-font-awesome blueman pulseaudio-bluetooth #bluetooth 
     }
   else
     {
