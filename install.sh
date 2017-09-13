25887b19a4840f21261c960588460a559104b096
Exocen <Exocen@users.noreply.github.com>
2017-09-13 23:46:59 +0200

    apt-get to apt

--- install.sh
+++ install.sh
@@ -65,8 +65,8 @@ function ins {
   all="$@" # pour fonction is_working
   echo "Installation: $all ...."
   if [ "$WOS" = "Ubuntu" ] || [ "$WOS" = "Debian" ] ;then
-    sudo apt-get update -y > /dev/null 2>&1
-    sudo apt-get install $@ -y # > /dev/null 2>&1
+    sudo apt update -y > /dev/null 2>&1
+    sudo apt install $@ -y # > /dev/null 2>&1
     is_working "Installation de $all"
   elif [ "$WOS" = "Fedora" ] ;then
     sudo dnf update -y #> /dev/null 2>&1
