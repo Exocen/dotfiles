edd4677b7e23ed16cd7898866f6eb6d97501c1cf
Exocen <Exocen@users.noreply.github.com>
2017-09-19 18:47:47 +0200

    ref install auto nginx + move bot config in right repo

--- tmpconf/install.sh
+++ tmpconf/install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-git pull https://github.com/hangoutsbot/hangoutsbot ~
+git clone https://github.com/exocen/hangoutsbot ~/hangoutsbot
+
+apt install nginx letencrypt -y
+mkdir -p /etc/nginx/ssl
+touch /etc/nginx/ssl/ticket.key
+openssl rand 48 >> /etc/nginx/ssl/ticket.key
+openssl dhparam -out /etc/nginx/ssl/dhparam4.pem 4096
+
+# certbot certonly --agree-tos --rsa-key-size 4096 --register-unsafely-without-email --webroot --webroot-path /var/www/html -d mondomaine.fr
+# crontab -e
+# 30 2 * * * certbot renew >> /var/log/letsencrypt/renewal.log
 
 # Local Variables:
 # mode: Shell-script
