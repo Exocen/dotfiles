98ab7e60b9ecf54ad91a45f9464b81721f4c6bcb
Exocen <Exocen@users.noreply.github.com>
2017-12-14 00:14:36 +0100

    zip all

--- server_https_mail/pre-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -s /etc/ca-certificates/extracted/ca-bundle.trust.crt /etc/ssl/certs/ca-certificates.crt
-git clone https://github.com/exocen/hangoutsbot ~
-tput setaf 1;tput setab 7; echo "bothangout@ bender"
-# Local Variables:
-# mode: Shell-script
-# coding: mule-utf-8
-# End:
