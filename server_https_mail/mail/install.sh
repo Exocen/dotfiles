673a87f1a161afd33e717f4117be81b6466e6b85
Exocen <Exocen@users.noreply.github.com>
2017-12-14 22:10:16 +0100

    interative stuff

--- server_https_mail/mail/install.sh
+++ server_https_mail/mail/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo hostnamectl set-hostname exocen.com
+sudo hostnamectl set-hostname HOSTNAME
 yaourt -Sy postfix --noconfirm
 sudo /bin/cp /etc/postfix/main.conf -f
 sudo /bin/cp /etc/postfix/master.cf -f
@@ -12,9 +12,9 @@ sudo postalias /etc/postfix/aliases
 yaourt -Sy python-postfix-policyd-spf opendkim --noconfirm
 
 sudo /bin/cp opendkim.conf /etc/opendkim/opendkim.conf -f
-sudo opendkim-genkey -r -s myselector --directory=/etc/opendkim/ -d exocen.com
+sudo opendkim-genkey -r -s myselector --directory=/etc/opendkim/ -d HOSTNAME
 # cp /etc/postfix/virtual6
-# nom@exocen.com mail
+# nom@HOSTNAME mail
 # postmap /etc/postfix/virtual
 # cat /etc/opendkim/myselector.txt
 sudo systemctl enable postfix opendkim
--- server_https_mail/pre-install.sh
+++ server_https_mail/pre-install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 # ln -s /etc/ca-certificates/extracted/ca-bundle.trust.crt /etc/ssl/certs/ca-certificates.crt
-git clone https://github.com/exocen/hangoutsbot ~
-tput setaf 1;tput setab 7; echo "bothangout@ bender install en root"
+# git clone https://github.com/exocen/hangoutsbot ~
+tput setaf 1;tput setab 7; echo "bothangout@ bender install";tput sgr0
+
+echo "Hostname ? :"
+read hostname
+echo $hostname" ? (Y/N)"
+read answer
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ] || [ "$answer" == "YES" ] || [ "$answer" == "yes" ];then
+    echo 'Pre-install.....'
+    sudo hostnamectl set-hostname $hostname
+    sed -i 's/HOSTNAME/'$hostname'/g' website/default website/default_https website/install_web.sh mail/install.sh mail/main.cf mail/opendkim.conf 
+else
+    echo 'Cancel'
+fi
+
 # Local Variables:
 # mode: Shell-script
 # coding: mule-utf-8
