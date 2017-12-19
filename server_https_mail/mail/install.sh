266e306e6bb6013b8db1d60dc4fcf09c959775e9
Exocen <Exocen@users.noreply.github.com>
2017-12-19 22:53:31 +0100

    REF mail install setup

--- server_https_mail/mail/install.sh
+++ server_https_mail/mail/install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 sudo hostnamectl set-hostname HOSTNAME
-yaourt -Sy postfix --noconfirm
+yaourt -Sy postfix python-postfix-policyd-spf opendkim --noconfirm
 sudo /bin/cp main.cf /etc/postfix/main.cf -f
 sudo /bin/cp master.cf /etc/postfix/master.cf -f
-# openssl dhparam -out /etc/postfix/dhparam.pem 2048 # si aucun ssl
 sudo postalias /etc/postfix/aliases
-
-# cp .forward ?
-
-yaourt -Sy python-postfix-policyd-spf opendkim --noconfirm
+# openssl dhparam -out /etc/postfix/dhparam.pem 2048 # if no ssl
 
 sudo /bin/cp opendkim.conf /etc/opendkim/opendkim.conf -f
 sudo opendkim-genkey -r -s myselector --directory=/etc/opendkim/ -d HOSTNAME
-# cp /etc/postfix/virtual6
-# nom@HOSTNAME mail
-# postmap /etc/postfix/virtual
-# cat /etc/opendkim/myselector.txt
+
 sudo systemctl enable postfix opendkim
 sudo systemctl start postfix opendkim
 
+# DKIM selector value : 
+# cat /etc/opendkim/myselector.txt
+
+# Mail redirection :
+# add "name@HOSTNAME mail-to-go" to end of /etc/postfix/virtual
+# Ex bob@bob.com bob@gmail.com
+# then
+# postmap /etc/postfix/virtual
