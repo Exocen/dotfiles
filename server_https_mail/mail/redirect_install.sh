ab388b4d84575e4d027b9d3138fa3f10fa2e8c5c
Exocen <Exocen@users.noreply.github.com>
2017-12-22 22:31:57 +0100

    auto redirect corr

--- server_https_mail/mail/redirect_install.sh
+++ server_https_mail/mail/redirect_install.sh
@@ -6,6 +6,7 @@ echo $email" ? (y/N)"
 read answer
 if [ "$answer" == "Y" ] || [ "$answer" == "y" ] || [ "$answer" == "YES" ] || [ "$answer" == "yes" ];then
     sudo bash -c 'echo "@$HOSTNAME "'$email' >> /etc/postfix/virtual'
+    sudo bash -c ' echo "virtual_alias_maps = hash:/etc/postfix/virtual" >> /etc/postfix/main.cf'
     sudo postmap /etc/postfix/virtual
     sudo systemctl restart postfix
     echo 'Finish'
