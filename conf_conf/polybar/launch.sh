efd304f3306ed609c7aa4908fef9b2855b21394f
Exocen <Exocen@users.noreply.github.com>
2020-02-04 21:53:42 -0600

    added mpd socket use

--- conf_conf/polybar/launch.sh
+++ conf_conf/polybar/launch.sh
@@ -2,7 +2,6 @@
 
 # Terminate already running bar instances
 killall -q polybar
-MPD_HOST="$HOME/.mpd/socket"
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
