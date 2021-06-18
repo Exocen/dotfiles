6975fb749e2bcf58fb6eacb28d81fc13a28e1898
Exocen <Exocen@users.noreply.github.com>
2021-06-18 01:01:47 -0400

    Add debug logs

--- manual_conf/web-monitor/website-monitor.py
+++ manual_conf/web-monitor/website-monitor.py
@@ -14,8 +14,19 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 file_path = os.path.dirname(os.path.realpath(__file__))
-# TODO add timestamp + logs
+lame_log = []
+debug = False
+# TODO add debug logs + debug run (--dry-run)
 
+def run_process(command):
+    if not debug:
+        subprocess.run(command, shell=True,check=True, executable="/bin/bash")
+
+def add_to_log(message):
+    lame_log.append(message)
+
+def get_log():
+    print(', '.join(lame_log))
 
 def backup_file(file_path):
     new_name = str(int(datetime.today().timestamp()))
@@ -76,16 +87,15 @@ def check_reserv(driver):
         backup_file(img_path)
         new_img.save(img_path)
         bash_scp = "scp "+img_path+" exo@exocen.com:/tmp/"
-        subprocess.run(bash_scp, shell=True, check=True,
-                       executable="/bin/bash")
+        run_process(bash_scp)
         message = "Website update"
         bash2 = "sendemail -m '"+message + \
             "' -t chaton@exocen.com -bcc check@exocen.com -u 'TRAIL CAMP UPDATE' -f exo@exocen.com -a /tmp/"+img_filename+""
         bashCommand = 'ssh exo@exocen.com "' + bash2 + '"'
-        subprocess.run(bashCommand, shell=True,
-                       check=True, executable="/bin/bash")
+        run_process(bashCommand)
+        add_to_log('Skyline New File')
     else:
-        print('Skyline No changes')
+        add_to_log('Skyline No changes')
 
 
 def check_reserv2(driver):
@@ -127,16 +137,15 @@ def check_reserv2(driver):
         backup_file(img_path)
         new_img.save(img_path)
         bash_scp = "scp "+img_path+" exo@exocen.com:/tmp/"
-        subprocess.run(bash_scp, shell=True, check=True,
-                       executable="/bin/bash")
+        run_process(bash_scp)
         message = "Website update"
         bash2 = "sendemail -m '"+message + \
             "' -t chaton@exocen.com -bcc check@exocen.com -u 'TRAIL CAMP UPDATE' -f exo@exocen.com -a /tmp/"+img_filename+""
         bashCommand = 'ssh exo@exocen.com "' + bash2 + '"'
-        subprocess.run(bashCommand, shell=True,
-                       check=True, executable="/bin/bash")
+        run_process(bashCommand)
+        add_to_log('Maligne New File')
     else:
-        print('Maligne No changes')
+        add_to_log('Maligne No changes')
 
 
 def check_smbc(driver):
@@ -167,24 +176,24 @@ def check_smbc(driver):
         backup_file(img_path)
         contentToFile(response.content, img_path)
         bash_scp = "scp "+img_path+" exo@exocen.com:/tmp/"
-        subprocess.run(bash_scp, shell=True, check=True,
-                       executable="/bin/bash")
+        run_process(bash_scp)
         message = "Website update"
         bash2 = "sendemail -m '"+message + \
             "' -t wesh@exocen.com -bcc check@exocen -u 'smbc UPDATE' -f exo2@exocen.com -a /tmp/"+img_filename+""
         bashCommand = 'ssh exo@exocen.com "' + bash2 + '"'
-        subprocess.run(bashCommand, shell=True,
-                       check=True, executable="/bin/bash")
+        run_process(bashCommand)
+        add_to_log('SMBC New File')
     else:
-        print('SMBC No changes')
+        add_to_log('SMBC No changes')
 
 
 try:
     driver = init()
     check_reserv(driver)
     check_reserv2(driver)
-    check_smbc(driver)
+    check_smbc(driver)    
 except Exception:
     raise
 finally:
+    get_log()
     driver.quit()
