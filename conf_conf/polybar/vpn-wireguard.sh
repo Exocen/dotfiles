8b5c4cd9581741f418856a401f2310f798d4911f
Exocen <Exocen@users.noreply.github.com>
2021-07-15 16:38:57 -0400

    added wireguard polybar conf

---
+++ conf_conf/polybar/vpn-wireguard.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#TODO add multiple config + click to disconnect or change
+
+connection_status() {
+    connection=$(sudo wg show 2>/dev/null | head -n 1 | awk '{print $NF }')
+
+    if [[ ! -z "$connection" ]]; then
+        echo "1"
+    else
+        echo "2"
+    fi
+}
+
+
+if [ "$(connection_status)" = "1" ]; then
+    echo "%{u#808080}%{+u}%{F#808080}ﱾ%{F-}"
+else
+    echo
+fi
