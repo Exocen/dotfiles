19fe9a774d56fa84d74251cb6fd2e09798baaf64
Exocen <Exocen@users.noreply.github.com>
2021-08-21 16:21:06 -0400

    T: added mysql cli

---
+++ manual_conf/lxc-sample/start_cont.sh
@@ -0,0 +1,1 @@
+export CONT=`date +%s | sha256sum | base64 | head -c 32 ; echo` && lxc launch images:debian/11/cloud $CONT -p cloud-profile && lxc exec $CONT -- cloud-init status -w && lxc exec $CONT -- su --login exo
