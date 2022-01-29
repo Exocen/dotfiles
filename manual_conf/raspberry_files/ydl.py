62c03b108b1c1310eadab429fb955e8af902ab98
Exocen <Exocen@users.noreply.github.com>
2022-01-29 02:41:35 -0500

    added bg

--- manual_conf/raspberry_files/ydl.py
+++ manual_conf/raspberry_files/ydl.py
@@ -6,43 +6,80 @@ import os.path
 import sys
 
 t = "PLAecd6qz5DFlaOXEmNifThzG-8mOSyyKU"
-filename = None
+audio_format = "flac"
+audio_data_list = []
+
+#create and delete this
+temp_dest = '~/tmp_files'
 
 year = date.today().strftime('%Y')
 
-
-def convert(filename):
-    if filename is not None:
-        filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
-        x = re.findall('(.*?)\s*-\s*(.*?)\..*', filename)
-        if len(x[0]) == 2:
-            x = x[0]
-            try:
-                meta = mutagen.easyid3.EasyID3(filepath)
-            except mutagen.id3.ID3NoHeaderError:
-                meta = mutagen.File(filepath, easy=True)
-            meta['title'] = x[1]
-            meta['artist'] = x[0]
-            meta.save()
-
-
-def my_hook(d):
-    if d['status'] == 'finished':
-        global filename
-        filename = os.path.splitext(d['filename'])[0]+'.flac'
-
-
 ydl_opts = {
-    'metafromtitle': '%(artist)s - %(title)',
+    # 'metafromtitle': '%(artist)s - %(title)',
     'extractaudio': True,
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'flac',
+        'preferredcodec': audio_format,
     }],
-    'outtmpl': '%(title)s.%(ext)s',
-    'progress_hooks': [my_hook],
+    # 'outtmpl': '%(title)s.%(ext)s',
+    # 'progress_hooks': [my_hook],
 }
+# todo rem outtmpl and metafromtile
+
+class audio_data:
+    artist = ''
+    filepath = ''
+
+    def __init__(self, title, yid):
+        self.title = title
+        self.yid = yid
+        parsed_title = re.findall('(.*?)\s*-\s*(.*?)\..*', title)
+        if len(parsed_title) > 0 and len(parsed_title[0]) == 2:
+            self.artist = parsed_title[0][0]
+            self.title = parsed_title[0][1]
+
+    def get_filename(self):
+        if not self.artist:
+            return self.title + '.' + audio_format
+        return self.artist + ' - ' + self.title + '.' + audio_format
+
+
+#dl all one with
+def dl_list(audio_datas):
+    # getfilepath
+    pass
+
+
+def tag_and_move(audio_data):
+
+    if audio_data.filepath:
+        if audio_data.artist:
+            filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+            try:
+               meta = mutagen.easyid3.EasyID3(filepath)
+            except mutagen.id3.ID3NoHeaderError:
+                meta = mutagen.File(filepath, easy=True)
+                meta['title'] = audio_data.title
+                meta['artist'] = audio_data.artist
+                meta.save()
+        #move file BUT NOT OVERWRITE
+
+
+def sort_stuff(title):
+    # full dict_title_by_id
+    # for filename in dict_title_by_audio:
+    # compare get_filename with existing files or csv list
+    # take the rest and compare with
+    # dict_title_by_audio.pop(title, None)
+    pass
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        # find and select audiodata , then add filepath :
+        filename.append(os.path.splitext(d['filename'])[0]+'.'+audio_format)
+
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     result = ydl.extract_info(t, download=False)
