5b2909d87ef9764f0728af21517a82d8f5f4f5db
Exocen <Exocen@users.noreply.github.com>
2022-01-29 16:23:40 -0500

    added bg

--- manual_conf/raspberry_files/ydl.py
+++ manual_conf/raspberry_files/ydl.py
@@ -1,33 +1,32 @@
-from datetime import date
 import mutagen
 import youtube_dl
 import re
-import os.path
 import sys
+from os import path, environ
 
-t = "PLAecd6qz5DFlaOXEmNifThzG-8mOSyyKU"
+playlist_id = sys.args[0] if len(sys.argv) == 1 else None
 audio_format = "flac"
-audio_data_list = []
 
 # create and delete this
-temp_dest = '~/tmp_files'
-# file_list_path = /run/user/$uid/file_list
+temp_dest = '/run/user/' + environ['UID'] + '/'
 
-year = date.today().strftime('%Y')
+filepath_by_id = dict()
+
+def my_hook(v):
+    if v['status'] == 'finished':
+        # global filepath_by_id
+        filepath_by_id[v['id']] = v['FILELELELELELANME']
+        # find and select audiodata , then add filepath :
 
 ydl_opts = {
-    # 'metafromtitle': '%(artist)s - %(title)',
     'extractaudio': True,
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': audio_format,
     }],
-    # 'outtmpl': '%(title)s.%(ext)s',
-    # 'progress_hooks': [my_hook],
+    'progress_hooks': [my_hook],
 }
-# todo rem outtmpl and metafromtile
-
 
 class audio_data:
     artist = ''
@@ -36,7 +35,7 @@ class audio_data:
     def __init__(self, title, yid):
         self.title = title
         self.yid = yid
-        parsed_title = re.findall('(.*?)\s*-\s*(.*?)\..*', title)
+        parsed_title = re.findall(r'(.*?)\s*-\s*(.*?)\..*', title)
         if len(parsed_title) > 0 and len(parsed_title[0]) == 2:
             self.artist = parsed_title[0][0]
             self.title = parsed_title[0][1]
@@ -48,18 +47,18 @@ class audio_data:
 
 
 # dl all one with
-def dl_list(audio_datas):
+def dl_list():
     # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     #     ydl.download([t])
     # getfilepath
     pass
 
 
-def tag_and_move(audio_data):
+def tag_and_move(audio_data, playlist_name):
 
     if audio_data.filepath:
         if audio_data.artist:
-            filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+            filepath = path.join(path.dirname(path.abspath(__file__)), 'wdqqwd')
             try:
                 meta = mutagen.easyid3.EasyID3(filepath)
             except mutagen.id3.ID3NoHeaderError:
@@ -68,25 +67,33 @@ def tag_and_move(audio_data):
                 meta['artist'] = audio_data.artist
                 meta.save()
         # move file BUT NOT OVERWRITE
+        # move dest -> music playlist -> playlist name
+    # return succeded title_list[]
 
-
-def sort_stuff(title):
-    # full dict_title_by_id
-    # for filename in dict_title_by_audio:
-    # check file_list.csv exist if not generate (file list for dest)
-    # compare get_filename with file_list.csv
-    # keep not existing files
+def generate_file_list(file_path):
+    # if exist : load and return []
+    # else : read dir and return []
     pass
 
+def __main__():
+    # infos = extract_info()
+    # playlist title -> title_list.cvs -> temp_dest
+    # file_list_path = path.join(temp_dest, 'title_list'+'.cvs')
+    # audio_data_list = []
+    # for info in infos['entries']:
+    #   audio_data_list.append(audio_data(info['title'],info['id']))
+    # existing_title_list = generate_file_list(file_list_path)
+    # audio_data_list = filter(lambda a: a.title not in existing_title_list,audio_data_list)
+    # if audio_data_list:
+    #   download
+    #   for audio_data in audio_data_list:
+    #       audio_data.file_path = filepath_by_id[audio_data.yid]
+    #   existing_title_list = tag_and_move(audio_data_list)
+    # create/overwrite file_list_path with existing_title_list
+    pass
 
-def my_hook(d):
-    if d['status'] == 'finished':
-        # find and select audiodata , then add filepath :
-        pass
-
-
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    result = ydl.extract_info(t, download=False)
+with youtube_dl.YoutubeDL() as ydl:
+    result = ydl.extract_info(playlist_id, download=False)
     for k in result['entries']:
         print(k['id'])
 
