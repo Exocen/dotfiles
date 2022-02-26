046ba07e7f04c8f4f0c15f07e7bcca82db52fbe6
Exocen <Exocen@users.noreply.github.com>
2022-02-26 16:21:37 -0500

    refactoring + todo cleaning

--- manual_conf/raspberry_files/ydl.py
+++ manual_conf/raspberry_files/ydl.py
@@ -4,35 +4,26 @@ import csv
 import shutil
 import youtube_dl
 import mutagen
+from time import sleep
+from random import randint
 from mutagen.easyid3 import EasyID3
 from os import path, listdir
-from tempfile import mkdtemp
+from tempfile import TemporaryDirectory
 
+
+# TODO intern solution
+# TODO add vpn managment
+# TODO auto stop if too much fails (3 retries => stop timer)
+# TODO VPN 403 -> auto vpn switch
 if len(sys.argv) != 4:
     print("Usage ./Script tmpram-dir dest-dir id")
     quit()
 
 audio_format = "flac"
+rng_range = 30
 tmp_dir = sys.argv[-3]
-# Should be removed before successfull script end
-pytemp_dir = mkdtemp(dir=tmp_dir)
 playlist_id = sys.argv[-1]
 playlist_path_location = sys.argv[-2]
-
-ydl_opts = {
-    "extractaudio": True,
-    "format": "bestaudio/best",
-    "quiet": True,
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": audio_format,
-        }
-    ],
-    "outtmpl": pytemp_dir + "/%(title)s.%(ext)s",
-    "sleep_interval": 10,
-    "max_sleep_interval": 20,
-}
 
 
 class Audio_data:
@@ -47,12 +38,11 @@ class Audio_data:
         else:
             self.artist = None
             self.filename = self.title + "." + audio_format
-        self.filepath = path.join(pytemp_dir, self.filename)
 
 
-def dl_list(audio_data_list):
+def dl_list(audio_data, ydl_opts):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([audio_data.pid for audio_data in audio_data_list])
+        ydl.download(audio_data.pid)
 
 
 def extract_info():
@@ -60,20 +50,20 @@ def extract_info():
         return ydl.extract_info(playlist_id, download=False)
 
 
-def tag_and_copy(audio_data_list):
+def tag_and_copy(audio_data_list, pytemp_dir):
     for audio_data in audio_data_list:
-        if audio_data.filepath:
-            dest_path = path.join(playlist_path_location, audio_data.filename)
-            if audio_data.artist:
-                try:
-                    meta = EasyID3(audio_data.filepath)
-                except mutagen.id3.ID3NoHeaderError:
-                    meta = mutagen.File(audio_data.filepath, easy=True)
-                    meta["title"] = audio_data.title
-                    meta["artist"] = audio_data.artist
-                    meta.save()
-            if not path.exists(dest_path):
-                shutil.copyfile(audio_data.filepath, dest_path)
+        dest_path = path.join(playlist_path_location, audio_data.filename)
+        filepath = path.join(pytemp_dir, audio_data.filename)
+        if audio_data.artist:
+            try:
+                meta = EasyID3(audio_data.filename)
+            except mutagen.id3.ID3NoHeaderError:
+                meta = mutagen.File(filepath, easy=True)
+                meta["title"] = audio_data.title
+                meta["artist"] = audio_data.artist
+                meta.save()
+        if not path.exists(dest_path):
+            shutil.copyfile(filepath, dest_path)
 
 
 def generate_file_list(file_path):
@@ -90,6 +80,21 @@ def write_title_list(file_path):
     with open(file_path, "w", newline="") as csv_file:
         write = csv.writer(csv_file)
         write.writerow(existing_title_list)
+
+
+def gen_ydl_options(audio_format, tmpdirname):
+    return {
+        "extractaudio": True,
+        "format": "bestaudio/best",
+        "quiet": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": audio_format,
+            }
+        ],
+        "outtmpl": tmpdirname + "/%(title)s.%(ext)s",
+    }
 
 
 def main():
@@ -110,16 +115,23 @@ def main():
         filter(lambda a: a.filename not in existing_title_list, audio_data_list)
     )
 
-    # Dl and tag missing
+    # Dl and tag missing ydl_opts
     if audio_data_list:
-        # TODO using + loop (no bulk dl) + rng sleep + print title -> logs ?
-        dl_list(audio_data_list)
-        tag_and_copy(audio_data_list)
+        with TemporaryDirectory(dir=tmp_dir) as tmpdirname:
+            # TODO print title -> logs ?
+            try:
+                for audio_data in audio_data_list:
+                    dl_list(audio_data, gen_ydl_options(audio_format, tmpdirname))
+                    # if not last occurence
+                    if audio_data != audio_data_list[-1]:
+                        sleep(randint(0, rng_range))
+                tag_and_copy(audio_data_list, tmpdirname)
+            # TODO add 403
+            except Exception as e:
+                print(e)
 
     # Create save
     write_title_list(file_list_path)
-
-    shutil.rmtree(pytemp_dir)
 
 
 if __name__ == "__main__":
