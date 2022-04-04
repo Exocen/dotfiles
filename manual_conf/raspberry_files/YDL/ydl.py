1316ab016d5506689884f158a3797eda906e36b7
Exocen <Exocen@users.noreply.github.com>
2022-04-04 16:09:55 -0400

    raspberry refactor

--- manual_conf/raspberry_files/YDL/ydl.py
+++ manual_conf/raspberry_files/YDL/ydl.py
@@ -8,7 +8,6 @@ import subprocess
 import os
 from time import sleep
 from random import randint
-from pathlib import Path
 from mutagen.easyid3 import EasyID3
 from os import path
 from tempfile import TemporaryDirectory
@@ -63,7 +62,7 @@ def tag_and_copy(audio_data, pytemp_dir):
             meta["artist"] = audio_data.artist
             meta.save()
     if not path.exists(dest_path):
-        shutil.copyfile(Path(filepath), Path(dest_path))
+        shutil.copyfile(filepath, dest_path)
 
 
 def generate_file_list(file_path):
