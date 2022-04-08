132226a98784b37b1be92d7c002a38b4e77b6b12
Exocen <Exocen@users.noreply.github.com>
2022-04-07 23:01:11 -0400

    ydl : added param list fix 3

--- manual_conf/raspberry_files/YDL/ydl.py
+++ manual_conf/raspberry_files/YDL/ydl.py
@@ -148,10 +148,9 @@ class Main:
         while (self.loop):
             try:
                 for params in self.params_list:
-                    param_list = ''.join(params).split(',')
-                    self.tmp_dir = param_list[0]
-                    self.playlist_id = param_list[1]
-                    self.playlist_path_location = param_list[2]
+                    self.tmp_dir = params[0]
+                    self.playlist_id = params[1]
+                    self.playlist_path_location = params[2]
                     self.downloader()
                     sleep(cooldown + randint(0, cooldown))
             except Network_Error:
