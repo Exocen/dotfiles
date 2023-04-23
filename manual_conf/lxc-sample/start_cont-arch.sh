c972bc90cd6b3640fbfcbfca2a10a2efbea7fa35
Exocen <Exocen@users.noreply.github.com>
2023-04-23 03:55:05 -0400

    Update start_cont-arch.sh

--- manual_conf/lxc-sample/start_cont-arch.sh
+++ manual_conf/lxc-sample/start_cont-arch.sh
@@ -2,4 +2,4 @@
 export CONT="archlinux-$(date '+%y%m%d-%H%M%S-%2N')" &&
     lxc launch images:archlinux/current/cloud $CONT -p cloud-profile &&
     lxc exec $CONT -- cloud-init status -w &&
-    lxc exec $CONT -- su --login exo
+    lxc exec $CONT -- su --login tester
