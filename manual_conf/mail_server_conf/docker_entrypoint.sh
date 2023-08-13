bcfa94401bf4507ef46a512b50c3b2a44cd330e5
Exocen <Exocen@users.noreply.github.com>
2023-08-13 00:02:13 -0600

    added dockerfiles intructions

--- manual_conf/mail_server_conf/docker_entrypoint.sh
+++ manual_conf/mail_server_conf/docker_entrypoint.sh
@@ -1,5 +1,8 @@
 certbot certonly -n --keep --standalone --register-unsafely-without-email --agree-tos -d $1
 
+service rsyslog restart
 service postfix restart
 service dovecot restart
 service opendkim restart
+
+tail -f /etc/mail.log
