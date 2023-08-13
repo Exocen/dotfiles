caccc1814683bba153287b9f0a7580eeb047325b
Exocen <Exocen@users.noreply.github.com>
2023-08-13 01:51:22 -0600

    added dockerfiles intructions

--- manual_conf/mail_server_conf/docker_entrypoint.sh
+++ manual_conf/mail_server_conf/docker_entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 certbot certonly -n --keep --standalone --register-unsafely-without-email --agree-tos -d [DOMAIN]
 
-service postfix stop
+service postfix restart
 service dovecot restart
 service opendkim restart
-(crontab -l 2>/dev/null; echo "@weekly certbot renew") | crontab -
-service crond restart
 
-postfix start-fg
+(crontab -l 2>/dev/null; echo "@weekly certbot renew") | crontab -
+cron -f
