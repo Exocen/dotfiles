1b17f3916885afc470b00a65885df6cca213ebbb
Exocen <Exocen@users.noreply.github.com>
2023-08-13 20:37:40 -0600

    added dockerfiles intructions

--- manual_conf/mail_server_conf/docker-run.sh
+++ manual_conf/mail_server_conf/docker-run.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-function main(){
-
-    docker build --build-arg DOMAIN=$1 -t mailserv . \
-\
-\  &&  docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /post_base:/post_base -v /var/mail/vhosts:/var/mail/vhosts -p 25:25 -p 587:587 -p 465:465 -p 143:143 -p 993:993 -p 80:80 --name mailserver -d --restart unless-stopped mailserv:latest
-
-}
-
 if [ `id -u` -ne 0 ]; then
     echo "Must be run as root"
+    exit 1
 else
     if [ -z "$1" ]; then
         echo "No domain supplied"
-    else
-        main $1
+        exit 1
     fi
 fi
+
+docker build --build-arg DOMAIN=$1 -t mailserv . \
+    \
+    &&  docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /post_base:/post_base -v /var/mail/vhosts:/var/mail/vhosts -p 25:25 -p 587:587 -p 465:465 -p 143:143 -p 993:993 -p 80:80 --name mailserver -d --restart unless-stopped mailserv:latest
 
