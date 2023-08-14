bd90066ad6f3e4a673b473fa5a802b43eae71c8c
Exocen <Exocen@users.noreply.github.com>
2023-08-13 22:04:53 -0600

    added dockerfiles intructions

--- manual_conf/mail_server_conf/docker-run.sh
+++ manual_conf/mail_server_conf/docker-run.sh
@@ -10,7 +10,7 @@ else
     fi
 fi
 
-docker build --build-arg DOMAIN=$1 -t mailserv . \
+docker build --build-arg DOMAIN=$1 -t mailserv . && \
     \
-    &&  docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /post_base:/post_base -v /var/mail/vhosts:/var/mail/vhosts -p 25:25 -p 587:587 -p 465:465 -p 143:143 -p 993:993 -p 80:80 --name mailserver -d --restart unless-stopped mailserv:latest
+    docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /post_base:/post_base -v /var/mail/vhosts:/var/mail/vhosts -p 25:25 -p 587:587 -p 465:465 -p 143:143 -p 993:993 --name mailserver -d --restart unless-stopped mailserv:latest
 
