e4c29c2732e548275a3369602b80bc117e0aa94a
Exocen <Exocen@users.noreply.github.com>
2023-08-14 21:42:53 -0600

    We Had To Use Dark Magic To Make This Work

--- manual_conf/nginx_certbot/docker-entrypoint.sh
+++ manual_conf/nginx_certbot/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 service syslog-ng restart
-echo "@daily /usr/bin/certbot renew --nginx &>/var/log/certbot.log"
+
+(echo "@daily /usr/bin/certbot renew --nginx &>/var/log/certbot.log") | crontab -
 service cron restart
 
+mkdir -p /var/log/nginx
 nginx -g 'daemon off;'
