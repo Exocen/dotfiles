3459cca0f14ed625d7b77130c99373b71c53b474
Exocen <Exocen@users.noreply.github.com>
2023-08-16 19:09:23 -0600

    It only compiles every 2 tries... good luck.

--- manual_conf/nginx_certbot/docker-run.sh
+++ manual_conf/nginx_certbot/docker-run.sh
@@ -14,5 +14,7 @@ docker stop nginx_certbot
 docker rm nginx_certbot
 docker build --build-arg DOMAIN=$1 -t nginx_certbot_img . && \
     \
-    docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /var/log/nginx_certbot:/var/log -p 80:80 -p 443:443 --name nginx_certbot -d --restart unless-stopped nginx_certbot_img:latest
+    docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /var/log/nginx_certbot:/var/log -p 80:80 -p 443:443 --name nginx_certbot -d --rm  nginx_certbot_img:latest &&\
+    docker exec -it nginx_certbot bash
+    #docker run -v /etc/letsencrypt/:/etc/letsencrypt/ -v /var/log/nginx_certbot:/var/log -p 80:80 -p 443:443 --name nginx_certbot -d --restart unless-stopped nginx_certbot_img:latest
 
