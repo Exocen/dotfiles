20944f4da7e412726bfbdbf1c764ca40ff6846ee
Exocen <Exocen@users.noreply.github.com>
2023-08-16 19:45:39 -0600

    dirty hack, have a better idea ?

--- manual_conf/nginx_certbot/docker-entrypoint.sh
+++ manual_conf/nginx_certbot/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+if certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]' ; then
+    certbot --nginx --keep --quiet --register-unsafely-without-email --agree-tos  -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] &>/var/log/certbot.log || echo "certbot failed exiting..." && exit 1
+    cp -fr /root/nginx.conf /etc/nginx/
+fi
+
 service syslog-ng restart
 
 (echo "@daily /usr/bin/certbot renew --nginx &>/var/log/certbot.log") | crontab -
