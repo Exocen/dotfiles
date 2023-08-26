141ffb1927fb2b4678314c13daf51354ebf16dfc
Exocen <Exocen@users.noreply.github.com>
2023-08-25 23:10:27 -0600

    Fixed a bug in NoteLineCount... not seriously...

---
+++ manual_conf/fail2ban/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "Must be run as root"
+    exit 1
+fi
+fail2ban-client -V &>/dev/null
+if  [ $? != 0 ]; then
+    echo "fail2ban must be installed"
+    exit 1
+fi
+
+cd $(dirname "$0")
+
+cp -fr filter/* /etc/fail2ban/filter.d/
+cp -fr jail/* /etc/fail2ban/jail.d/
+mkdir -p /var/log/docker/mail_server
+mkdir -p /var/log/docker/vaultwarder
+touch /var/log/docker/mail_server/dovecot.log
+touch /var/log/docker/mail_server/mail.log
+touch /var/log/docker/vaultwarder/vaultwarden.log
+
+systemctl restart fail2ban
