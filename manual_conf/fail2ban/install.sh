e05ed9b686a03ef54f43bd985b5687a84293ce97
Exocen <Exocen@users.noreply.github.com>
2023-08-25 23:23:32 -0600

    No changes after this point.

--- manual_conf/fail2ban/install.sh
+++ manual_conf/fail2ban/install.sh
@@ -12,12 +12,13 @@ fi
 
 cd $(dirname "$0")
 
+cp -fr logrotate_docker /etc/logrotate.d/
 cp -fr filter/* /etc/fail2ban/filter.d/
 cp -fr jail/* /etc/fail2ban/jail.d/
 mkdir -p /var/log/docker/mail_server
-mkdir -p /var/log/docker/vaultwarder
+mkdir -p /var/log/docker/vaultwarden
 touch /var/log/docker/mail_server/dovecot.log
 touch /var/log/docker/mail_server/mail.log
-touch /var/log/docker/vaultwarder/vaultwarden.log
+touch /var/log/docker/vaultwarden/vaultwarden.log
 
 systemctl restart fail2ban
