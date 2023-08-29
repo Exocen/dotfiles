721b2c9f95eebae59b51080c1a1a807343378cfa
Exocen <Exocen@users.noreply.github.com>
2023-08-29 00:19:44 -0600

    We'll figure it out on Monday

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-
-certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
+/usr/bin/certbot certificates --max-log-backups 0 | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-    certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] &>/var/log/certbot.log || (echo "certbot failed exiting..." && exit 1)
+    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] --max-log-backups 0 || (echo "certbot failed exiting..." && exit 1)
 fi
 
 cp -fr /root/nginx.conf /etc/nginx/
 service syslog-ng restart
 
-(echo "@daily /usr/bin/certbot renew --nginx &>/var/log/certbot.log") | crontab -
+echo "/usr/bin/certbot renew --nginx --max-log-backups 0" > /usr/bin/certbot-renewer
+chmod +x /usr/bin/certbot-renewer
+(echo "@daily /usr/bin/certbot-renewer") | crontab -
 service cron restart
 
 mkdir -p /var/log/nginx
