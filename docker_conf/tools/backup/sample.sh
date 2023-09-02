169bac23912f14c6378733deb560ee4525b4709c
Exocen <Exocen@users.noreply.github.com>
2023-09-01 23:35:28 -0600

    It's secret!

--- docker_conf/tools/backup/sample.sh
+++ docker_conf/tools/backup/sample.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-tar cvzf - /mail_server-data /ng-data /vw-data | gpg -c --passphrase test --pinentry-mode loopback > backdown
-ls -l
-gpg -d --pinentry-mode loopback --passphrase test backdown | tar -xvzf -
-ls -l
-
-
-ssh HOST sudo tar czf - /mail_server-data /vw-data /ng-data > test.tgz
+ssh HOST sudo tar czf - /docker-data > test.tgz
