06b05cce35aad60e22db64ead02085ef5f531654
Exocen <Exocen@users.noreply.github.com>
2023-09-03 01:30:52 -0600

    Done, to whoever merges this, good luck.

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -3,16 +3,19 @@ CERTBOT_RENEW_DATE='03:14 tomorrow'
 
 certbot_renew(){
     echo "Starting Certbot renew..."
-    /usr/bin/certbot renew --nginx --logs-dir /dev/null
+    /usr/bin/certbot renew --nginx
     echo "Will restart at $CERTBOT_RENEW_DATE"
     sleep $(( $(date -d $CERTBOT_RENEW_DATE +%s) - $(date +%s) ))
 }
 
 mkdir -p /var/log/nginx
+mkdir -p /var/log/letsencrypt
+ln -s /dev/null /var/log/letsencrypt/letsencrypt.log
+
 /usr/bin/certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-    /usr/bin/certbot --nginx --keep-until-expiring --logs-dir /dev/null --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
+    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
 fi
 
 cp -fr /root/nginx.conf /etc/nginx/
