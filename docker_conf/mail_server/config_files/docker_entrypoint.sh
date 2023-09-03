1a0a7ccbf636dfb2b6590b2b54c70dd9d94e8dda
Exocen <Exocen@users.noreply.github.com>
2023-09-03 00:41:16 -0600

    docker: get syslog-ng back

--- docker_conf/mail_server/config_files/docker_entrypoint.sh
+++ docker_conf/mail_server/config_files/docker_entrypoint.sh
@@ -6,6 +6,7 @@ cp -rn /pre_base/* /post_base/
 chown -R vmail:vmail /post_base/vhosts
 chown opendkim:opendkim /post_base/mail.private
 
+service syslog-ng restart
 service dovecot restart
 service opendkim restart
 
--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 mkdir -p /var/log/nginx
-/usr/bin/certbot certificates --max-log-backups 0 | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
+/usr/bin/certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] --max-log-backups 0 || (echo "certbot failed exiting..." && exit 1)
+    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
 fi
 
-echo "@daily /usr/bin/certbot renew --nginx --max-log-backups 0" > /etc/cron.d/certbot
 service cron restart
 
 cp -fr /root/nginx.conf /etc/nginx/
