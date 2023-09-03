efddc04d222f7876eb9292f126fc306438cf697d
Exocen <Exocen@users.noreply.github.com>
2023-09-03 01:27:03 -0600

    docker mail_server : log to journald

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+CERTBOT_RENEW_DATE='03:14 tomorrow'
 
 certbot_renew(){
-    /usr/bin/certbot renew --nginx
-    sleep $(( $(date -d '05:00 tomorrow' +%s) - $(date +%s) ))
+    echo "Starting Certbot renew..."
+    /usr/bin/certbot renew --nginx --logs-dir /dev/null
+    echo "Will restart at $CERTBOT_RENEW_DATE"
+    sleep $(( $(date -d $CERTBOT_RENEW_DATE +%s) - $(date +%s) ))
 }
 
 mkdir -p /var/log/nginx
 /usr/bin/certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
+    /usr/bin/certbot --nginx --keep-until-expiring --logs-dir /dev/null --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
 fi
 
 cp -fr /root/nginx.conf /etc/nginx/
