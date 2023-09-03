638c00755e52f9b3cfec254eedb0dd67a562d84a
Exocen <Exocen@users.noreply.github.com>
2023-09-03 01:59:36 -0600

    i need therapy

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -2,10 +2,12 @@
 CERTBOT_RENEW_DATE='03:14 tomorrow'
 
 certbot_renew(){
-    echo "Starting Certbot renew..."
-    /usr/bin/certbot renew --nginx
-    echo "Will restart at $CERTBOT_RENEW_DATE"
-    sleep $(( $(date -d "$CERTBOT_RENEW_DATE" +%s) - $(date +%s) ))
+    while true; do
+        echo "Starting Certbot renew..."
+        /usr/bin/certbot renew --nginx
+        echo "Will restart at $CERTBOT_RENEW_DATE"
+        sleep $(( $(date -d "$CERTBOT_RENEW_DATE" +%s) - $(date +%s) ))
+    done
 }
 
 mkdir -p /var/log/nginx
@@ -15,9 +17,9 @@ ln -s /dev/null /var/log/letsencrypt/letsencrypt.log
 /usr/bin/certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]' &>/dev/null
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-   /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] &> >(sed "s/^/[Certbot] /") || (echo "certbot failed exiting..." && exit 1)
+    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
 fi
 
 cp -fr /root/nginx.conf /etc/nginx/
-certbot_renew &> >(sed "s/^/[Certbot] /") &
-nginx -g 'daemon off;' &> >(sed "s/^/[Nginx] / ")
+certbot_renew &
+nginx -g 'daemon off;'
