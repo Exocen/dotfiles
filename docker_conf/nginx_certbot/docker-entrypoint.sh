c7a58483ad4c5d8de761d37532936225d3a80037
Exocen <Exocen@users.noreply.github.com>
2023-09-03 01:38:59 -0600

    did everything

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -15,9 +15,9 @@ ln -s /dev/null /var/log/letsencrypt/letsencrypt.log
 /usr/bin/certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] || (echo "certbot failed exiting..." && exit 1)
+    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] | sed -e 's/^/Certbot: /;' || (echo "certbot failed exiting..." && exit 1)
 fi
 
 cp -fr /root/nginx.conf /etc/nginx/
-certbot_renew &
-nginx -g 'daemon off;'
+certbot_renew | sed -e 's/^/Certbot: /;' &
+nginx -g 'daemon off;' | sed -e 's/^/Nginx: /;'
