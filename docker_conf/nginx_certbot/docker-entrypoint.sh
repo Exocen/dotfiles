531581e4a6d7764377517d4ba688b4bf0918cf9e
Exocen <Exocen@users.noreply.github.com>
2023-09-03 01:45:05 -0600

    Derp, asset redirection in dev mode

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -8,6 +8,13 @@ certbot_renew(){
     sleep $(( $(date -d "$CERTBOT_RENEW_DATE" +%s) - $(date +%s) ))
 }
 
+function prefix_cmd {
+    local PREF="${1//\//\\/}" # replace / with \/
+    shift
+    local CMD=("$@")
+    ${CMD[@]} 1> >(sed "s/^/${PREF}/") 2> >(sed "s/^/${PREF}/" 1>&2)
+}
+
 mkdir -p /var/log/nginx
 mkdir -p /var/log/letsencrypt
 ln -s /dev/null /var/log/letsencrypt/letsencrypt.log
@@ -15,9 +22,9 @@ ln -s /dev/null /var/log/letsencrypt/letsencrypt.log
 /usr/bin/certbot certificates | grep 'vw.[DOMAIN]\|www.[DOMAIN]'
 RESULT=$?
 if [ $RESULT -eq 1 ]; then
-    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] | sed -e 's/^/Certbot: /;' || (echo "certbot failed exiting..." && exit 1)
+   prefix_cmd "Certbot: " /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -d [DOMAIN] -d vw.[DOMAIN] -d www.[DOMAIN] | sed -e 's/^/Certbot: /;' || (echo "certbot failed exiting..." && exit 1)
 fi
 
 cp -fr /root/nginx.conf /etc/nginx/
-certbot_renew | sed -e 's/^/Certbot: /;' &
-nginx -g 'daemon off;' | sed -e 's/^/Nginx: /;'
+prefix_cmd "Certbot: " certbot_renew
+prefix_cmd "Nginx: " nginx -g 'daemon off;'
