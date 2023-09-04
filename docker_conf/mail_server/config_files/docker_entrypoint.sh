79db49098dd9edf6de36897554a59bea9f111b9a
Exocen <Exocen@users.noreply.github.com>
2023-09-03 23:54:51 -0600

    I am sorry

--- docker_conf/mail_server/config_files/docker_entrypoint.sh
+++ docker_conf/mail_server/config_files/docker_entrypoint.sh
@@ -7,9 +7,9 @@ chown -R vmail:vmail /post_base/vhosts
 chown opendkim:opendkim /post_base/mail.private
 
 service syslog-ng restart
-service dovecot restart
-service opendkim restart
-service postfix restart
+(dovecot ; exit 1) &
+(opendkim ; exit 1) &
+(postfix ; exit 1) &
 
 tail -F /var/log/dovecot.log &
 tail -F /var/log/mail.log
