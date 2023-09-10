4acfb190557e9a00ff22ff896837db855f7e6a99
Exocen <Exocen@users.noreply.github.com>
2023-09-10 17:46:20 -0600

    docker backup: refactored sample.sh

--- docker_conf/tools/backup/sample.sh
+++ docker_conf/tools/backup/sample.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+HOST=[DOMAIN]
+OUTPUT=$HOST-docker-backup.tgz
 
-ssh HOST sudo tar czf - /docker-data > test.tgz
+ssh $HOST "cd / && tar cz docker-data" > $OUTPUT
+if [ $? -eq 0 ]; then
+    echo "Backup $OUTPUT created"
+    exit 0
+else
+    echo "Backup from $HOST failed"
+    exit 1
+fi
