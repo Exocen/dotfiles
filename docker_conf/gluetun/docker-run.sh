02731aaec453455469e388d58070ba0628c788c3
Exocen <Exocen@users.noreply.github.com>
2023-09-10 19:52:47 -0600

    these guys are flipped

--- docker_conf/gluetun/docker-run.sh
+++ docker_conf/gluetun/docker-run.sh
@@ -3,10 +3,29 @@
 if [ `id -u` -ne 0 ]; then
     echo "Must be run as root"
     exit 1
+else
+    if [ -z "$1" ]; then
+        echo "No mullvad id supplied"
+        exit 1
+    fi
 fi
 
-docker stop gluetun
-docker rm gluetun
+docker stop gluetun 2>/dev/null
+docker rm gluetun 2>/dev/null
 
-docker run -it --rm --cap-add=NET_ADMIN --name gluetun -e VPN_SERVICE_PROVIDER=mullvad -e VPN_TYPE=openvpn \
--e OPENVPN_USER=[USER] -e SERVER_COUNTRIES=[COUNTRIES] qmcgaw/gluetun
+docker run -d --rm --cap-add=NET_ADMIN --name gluetun -e VPN_SERVICE_PROVIDER=mullvad -e VPN_TYPE=openvpn \
+    -e OPENVPN_USER=$1 qmcgaw/gluetun
+
+# Optional environment variables
+
+#  -e  SERVER_COUNTRIES: Comma separated list of countries
+#  -e  SERVER_CITIES: Comma separated list of cities
+#  -e  SERVER_HOSTNAMES: Comma separated list of server hostnames
+#  -e  ISP: Comma separated list of ISPs
+#  -e  OWNED_ONLY: If the VPN server is owned by Mullvad. It defaults to no, meaning it includes all servers. It can be set to yes.
+#  -e  VPN_ENDPOINT_PORT: Custom OpenVPN server endpoint port to use
+#        For TCP: 80, 443 or 1401
+#        For UDP: 53, 1194, 1195, 1196, 1197, 1300, 1301, 1302, 1303 or 1400
+#        It defaults to 443 for TCP and 1194 for UDP
+#  -e  VPN_ENDPOINT_PORT: Custom Wireguard server endpoint port to use
+
--- docker_conf/ydl/docker-run.sh
+++ docker_conf/ydl/docker-run.sh
@@ -5,8 +5,8 @@ if [ `id -u` -ne 0 ]; then
     exit 1
 fi
 
-docker stop ydl
-docker rm ydl
+docker stop ydl 2>/dev/null
+docker rm ydl 2>/dev/null
 docker build -t ydl_img . && \
     \
     docker run --log-driver=journald -v /docker-data/ydl/:/ydl --name ydl -d --network=container:gluetun --restart unless-stopped ydl_img:latest
