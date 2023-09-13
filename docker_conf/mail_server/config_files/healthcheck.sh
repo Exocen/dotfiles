6746c9005c57659952ea1a4caf46bca760dd43e8
Exocen <Exocen@users.noreply.github.com>
2023-09-12 20:52:24 -0600

    Fixing Wade's bugs.

--- docker_conf/mail_server/config_files/healthcheck.sh
+++ docker_conf/mail_server/config_files/healthcheck.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#TODO send email and check
-#need to precreate test user
-# check with msmtp, other, or internal
 pidof opendkim 1>/dev/null && echo "opendkim is running." || exit 1
 service dovecot status && service postfix status || exit 1
