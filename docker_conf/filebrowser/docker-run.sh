2fb1e52825e8366546e63be245916588661f7a9b
Exocen <Exocen@users.noreply.github.com>
2023-09-19 15:59:31 -0600

    Does this work

--- docker_conf/filebrowser/docker-run.sh
+++ docker_conf/filebrowser/docker-run.sh
@@ -9,7 +9,7 @@ mkdir -p /docker-data/filebrowser/
 touch /docker-data/filebrowser/filebrowser.db
 docker run \
     --name filebrowser --log-driver=journald --rm -d \
-    -e FB_NOAUTH=noauth
+    -e FB_NOAUTH=noauth \
     -v /SSD_2T:/srv \
     -u $(id -u):$(id -g) \
     -p 80:80 \
