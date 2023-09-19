86643e31630cd19ccfa6b9fcc48bee4820e912d9
Exocen <Exocen@users.noreply.github.com>
2023-09-19 13:55:05 -0600

    why is everything broken

--- docker_conf/filebrowser/docker-run.sh
+++ docker_conf/filebrowser/docker-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 if [ `id -u` -ne 0 ]; then
-     echo "Must be run as root"
-     exit 1
- fi
+    echo "Must be run as root"
+    exit 1
+fi
 
+touch /docker-data/filebrowser/filebrowser.db
 docker run \
     --name filebrowser --log-driver=journald --rm -d \
     -v /SSD_2T:/srv \
-    -v /docker-data/filebrowser/:/root/ \
+    -v /docker-data/filebrowser/filebrowser.db:/database.db \
+    -v /docker-data/filebrowser/.filebrowser.json:/.filebrowser.json \
     -u $(id -u):$(id -g) \
     -p 8080:80 \
-    filebrowser/filebrowser -c /root/.filebrowser.json -d /root/filebrowser.db -r /root/data
+    filebrowser/filebrowser
