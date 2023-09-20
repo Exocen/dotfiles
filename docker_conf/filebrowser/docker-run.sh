ebe13b1891b9db2a98339e8bf6f6d484a9ee6869
Exocen <Exocen@users.noreply.github.com>
2023-09-19 18:44:06 -0600

    QuickFix.

--- docker_conf/filebrowser/docker-run.sh
+++ docker_conf/filebrowser/docker-run.sh
@@ -5,11 +5,19 @@ if [ `id -u` -ne 0 ]; then
     exit 1
 fi
 
+cd "$(dirname "$(readlink -f "$0")")"
 mkdir -p /docker-data/filebrowser/
 touch /docker-data/filebrowser/filebrowser.db
+
+
+filebrowser_path="/docker-data/filebrowser/filebrowser.json"
+[ -f "$filebrowser_path" ] || cp default_filebrowser.json $filebrowser_path
+
 docker run \
     --name filebrowser --log-driver=journald --rm -d \
     -e FB_NOAUTH=noauth \
+    -v /docker-data/filebrowser/filebrowser.db:/database/filebrowser.db \
+    -v $filebrowser_path:/.filebrowser.json \
     -v /SSD_2T:/srv \
     -u $(id -u):$(id -g) \
     -p 80:80 \
