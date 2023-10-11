08e92d2339bf2fdf9b2af3dfc00b1055e989740c
Exocen <Exocen@users.noreply.github.com>
2023-10-11 16:33:35 -0600

    It worked for me...

--- docker_conf/transmission/docker-run.sh
+++ docker_conf/transmission/docker-run.sh
@@ -3,6 +3,12 @@
 if [ `id -u` -ne 0 ]; then
     echo "Must be run as root"
     exit 1
+else
+    if [ -z ${TRANSMISSION_DL_PATH+x} ] ; then
+        if [ -z "$1" ]; then
+            TRANSMISSION_DL_PATH="/docker-data/transmission/dl/"
+        fi
+    fi
 fi
 
 docker run -d --rm --log-driver=journald \
@@ -12,5 +18,5 @@ docker run -d --rm --log-driver=journald \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
     --network=container:gluetun \
     -v /docker-data/transmission/config/:/config \
-    -v /docker-data/transmission/dl/:/downloads \
+    -v $TRANSMISSION_DL_PATH:/downloads \
     lscr.io/linuxserver/transmission:latest && echo "transmission started."
