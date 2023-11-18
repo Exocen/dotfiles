de5594e25595ec637954fca4d8a90e97a3e1cd86
Exocen <Exocen@users.noreply.github.com>
2023-11-18 05:32:15 -0500

    Last time I said it works? I was kidding.  Try this.

--- docker_conf/mail_server/docker-run.sh
+++ docker_conf/mail_server/docker-run.sh
@@ -22,7 +22,7 @@ docker run -d --rm --log-driver=journald \
     -v /docker-data/dms/mail-data:/var/mail -v /docker-data/dms/mail-state:/var/mail-state \
     -v /docker-data/dms/mail-logs:/var/log/mail -v /docker-data/dms/config:/tmp/docker-mailserver \
     -v /docker-data/letsencrypt:/etc/letsencrypt \
-    -p 464:465 -p 992:993 \
+    -p 25:25 -p 464:465 -p 992:993 \
     -e ENABLE_FAIL2BAN=1 -e SSL_TYPE=letsencrypt -e PERMIT_DOCKER=network \
     -e ONE_DIR=1 -e ENABLE_POSTGREY=0 -e ENABLE_CLAMAV=0 -e ENABLE_SPAMASSASSIN=0 -e SPOOF_PROTECTION=0 \
     --cap-add=NET_ADMIN \
