57ae7fd5f170f5e7579f5cd88bc5564c754cfa1d
Exocen <Exocen@users.noreply.github.com>
2023-11-18 18:52:37 -0500

    Merging the merge

--- docker_conf/snappymail/docker-run.sh
+++ docker_conf/snappymail/docker-run.sh
@@ -4,18 +4,13 @@ if [ `id -u` -ne 0 ]; then
     echo "Must be run as root"
     exit 1
 fi
-if [ -z ${DOMAIN+x} ] ; then
-    if [ -z "$1" ]; then
-        echo "No domain supplied"
-        exit 1
-    else
-        DOMAIN=$1
-    fi
-fi
+
+# First time conf + password change
+# mail.domain.com/?admin -> user= admin pass= /docker-data/snappymail/_data_/_default_/admin_password.txt
 
 docker run -d --rm --log-driver=journald \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
-    -v /docker-data/snappymail:/snappymail/data
+    -v /docker-data/snappymail:/snappymail/data \
     --net user_network --ip 10.0.0.82 \
     --name=snappymail \
     kouinkouin/snappymail && echo "Snappymail started."
