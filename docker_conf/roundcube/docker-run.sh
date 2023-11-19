3df03273160730b2d495b07b111ddf4b871e49fd
Exocen <Exocen@users.noreply.github.com>
2023-11-18 19:09:15 -0500

    breathe, =, breathe

---
+++ docker_conf/roundcube/docker-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "Must be run as root"
+    exit 1
+fi
+if [ -z ${DOMAIN+x} ] ; then
+    if [ -z "$1" ]; then
+        echo "No domain supplied"
+        exit 1
+    else
+        DOMAIN=$1
+    fi
+fi
+
+docker run -d --rm --log-driver=journald \
+    -e ROUNDCUBEMAIL_DEFAULT_HOST=ssl://$DOMAIN -e ROUNDCUBEMAIL_SMTP_SERVER=ssl://$DOMAIN \
+    -e ROUNDCUBEMAIL_SMTP_PORT=464 -e ROUNDCUBEMAIL_DEFAULT_PORT=992 \
+    -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
+    --net user_network --ip 10.0.0.82 \
+    --name=roundcube \
+    roundcube/roundcubemail && echo "Roundcube started."
