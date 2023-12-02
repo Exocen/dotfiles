21d7d0317394eec0969ed250333862f81812e80b
Exocen <Exocen@users.noreply.github.com>
2023-12-02 02:49:53 -0700

    docker : ydl reformatting

--- docker_conf/nginx_certbot/docker-entrypoint.sh
+++ docker_conf/nginx_certbot/docker-entrypoint.sh
@@ -17,6 +17,7 @@ mkdir -p $LOG_DIR
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
     /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos --logs-dir $LOG_DIR -q -d [DOMAIN] -d git.[DOMAIN] -d mail.[DOMAIN] -d status.[DOMAIN] -d www.[DOMAIN] -d vw.[DOMAIN]
+    /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null
     if [ $? -ne 0 ]; then
         echo "certbot failed exiting..."
         exit 1
