6cd4c385b9d270159c33761ffc8ef7984dba54f8
Exocen <Exocen@users.noreply.github.com>
2023-12-02 03:18:32 -0700

    docker : ydl reformatting

--- docker_conf/mail_server/setup-mail.sh
+++ docker_conf/mail_server/setup-mail.sh
@@ -5,5 +5,5 @@ if [ `id -u` -ne 0 ]; then
     exit 1
 fi
 
-[ ! -d "/docker-data/dms/config/opendkim" ] && docker exec -ti mail_server setup config dkim && docker restart mail_server
 docker exec -ti mail_server setup "$@"
+[ ! -d "/docker-data/dms/config/opendkim" ] && docker exec -ti mail_server setup config dkim && docker restart mail_server
