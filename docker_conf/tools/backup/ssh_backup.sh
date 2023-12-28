eb3f8533259cbec5f307292c4c2761b304e6c09f
Exocen <Exocen@users.noreply.github.com>
2023-12-28 03:09:16 -0700

    arrrggghhhhh fixed!

--- docker_conf/tools/backup/ssh_backup.sh
+++ docker_conf/tools/backup/ssh_backup.sh
@@ -8,8 +8,7 @@ MAX_BACKUP=10
 rotate_backup() {
     mkdir -p $BACKUP_DIR
     if [ `ls -rt $BACKUP_DIR | wc -l` -ge $MAX_BACKUP ] ; then
-        cd $BACKUP_DIR
-        rm -- "$(ls -rt $BACKUP_DIR | head -1)" && rotate_backup
+        rm -v -- $BACKUP_DIR/$(ls -rt $BACKUP_DIR | head -1) && echo "$to_remove removed" && rotate_backup
     fi
 }
 
