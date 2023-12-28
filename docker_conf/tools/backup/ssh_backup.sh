e1d8a002359fc059369493acbf4e6b9d2cc0af57
Exocen <Exocen@users.noreply.github.com>
2023-12-28 02:59:05 -0700

    Pig

--- docker_conf/tools/backup/ssh_backup.sh
+++ docker_conf/tools/backup/ssh_backup.sh
@@ -5,11 +5,14 @@ OUTPUT=$BACKUP_PREFFIX-`date +"%s"`.tgz
 BACKUP_DIR=$HOME/backup-$HOST
 MAX_BACKUP=10
 
-mkdir -p $BACKUP_DIR
-backup_to_remove_count=$((`ls -rta $BACKUP_DIR | wc -l` - $MAX_BACKUP))
-if [ $backup_to_remove_count -gt 0 ] ; then
-    rm -- "$(ls -rta $BACKUP_DIR | head -$backup_to_remove_count)"
-fi
+rotate_backup() {
+    mkdir -p $BACKUP_DIR
+    if [ `ls -rta $BACKUP_DIR | wc -l` -gt $MAX_BACKUP ] ; then
+        cd $BACKUP_DIR
+        rm -- "$(ls -rta $BACKUP_DIR | head -1)"
+        rotate_backup
+    fi
+}
 
 ssh $HOST "cd / && tar cz docker-data" > $BACKUP_DIR/$OUTPUT
 if [ $? -eq 0 ]; then
@@ -19,3 +22,4 @@ else
     echo "Backup from $HOST failed"
     exit 1
 fi
+rotate_backup
