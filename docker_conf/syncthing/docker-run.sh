4a9c1615c8c6de938576f93c9f75a0caf7b7298a
Exocen <Exocen@users.noreply.github.com>
2024-01-09 17:13:51 -0700

    docker: added syncthing + serv_manager conf

---
+++ docker_conf/syncthing/docker-run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "Must be run as root"
+    exit 1
+else
+    if [ -z ${SYNCTHING_PATH+x} ] ; then
+        if [ -z "$1" ]; then
+            SYNCTHING_PATH="/docker-data/syncthing/root/"
+        fi
+    fi
+fi
+
+docker run -d --rm --log-driver=journald \
+    --name=syncthing \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
+    -p 8384:8384 \
+    -p 22000:22000/tcp \
+    -p 22000:22000/udp \
+    -p 21027:21027/udp \
+    -v /docker-data/syncthing/config:/config \
+    -v $SYNCTHING_PATH:/data1 \
+    lscr.io/linuxserver/syncthing:latest && echo "syncthing started."
