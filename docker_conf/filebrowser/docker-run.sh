7cabfa4355238c42624fee84b2318dd35c6fdbd6
Exocen <Exocen@users.noreply.github.com>
2024-01-20 01:46:46 -0700

    Derp. Fix missing constant post rename

--- docker_conf/filebrowser/docker-run.sh
+++ docker_conf/filebrowser/docker-run.sh
@@ -16,18 +16,20 @@ fi
 
 cd "$(dirname "$(readlink -f "$0")")"
 mkdir -p /docker-data/filebrowser/
-touch /docker-data/filebrowser/filebrowser.db
 
-
-filebrowser_settings_path="/docker-data/filebrowser/filebrowser.json"
-[ -f "$filebrowser_settings_path" ] || cp default_filebrowser.json $filebrowser_settings_path
+DOCKER_PATH="/docker-data/filebrowser/"
+filebrowser_settings_path="$DOCKER_PATH/filebrowser.json"
+[ -f "$FILEBROWSER_SETTINGS_PATH" ] || cp default_filebrowser.json $FILEBROWSER_SETTINGS_PATH
+chown 1000:1000 $FILEBROWSER_SETTINGS_PATH
 
 docker run \
     --name filebrowser --log-driver=journald --rm -d \
     -e FB_NOAUTH=noauth \
-    -v /docker-data/filebrowser/filebrowser.db:/database/filebrowser.db \
-    -v $filebrowser_settings_path:/.filebrowser.json \
+    -v $DOCKER_PATH:/database \
+    -v $FILEBROWSER_SETTINGS_PATH:/.filebrowser.json \
     -v $FILEBROWSER_PATH:/srv \
+    -e PUID=1000 \
+    -e PGID=1000 \
     -u 1000:1000 \
     -p 8080:80 \
     filebrowser/filebrowser
