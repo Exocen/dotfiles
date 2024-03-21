4ddbe307ecc6dfcc6c46a8541f3eda073ac66194
Exocen <Exocen@users.noreply.github.com>
2024-03-21 16:34:21 -0600

    Check next commit for message.

--- docker_conf/plex/docker-run.sh
+++ docker_conf/plex/docker-run.sh
@@ -7,8 +7,10 @@ fi
 
 docker run -d --rm --log-driver=journald \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
+    --network=host
+    -e PLEX_UID=1000 \
+    -e PLEX_GID=1000 \
     -p 32400:32400/tcp \
-    -u 1000:1000 \
     -v /docker-data/plex:/data \
     --name=plex \
      plexinc/pms-docker:latest && echo "Plex started."
