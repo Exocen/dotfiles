5a3cce8b949891d5631d431706f08d2e9034ac6f
Exocen <Exocen@users.noreply.github.com>
2024-03-21 16:25:19 -0600

    after of this commit remember do a git reset hard

---
+++ docker_conf/plex/docker-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "Must be run as root"
+    exit 1
+fi
+if [ -z ${DOMAIN+x} ] ; then
+    if [ -z "$1" ]; then
+        echo "No domain supplied"
+        exit 1
+    else
+        DOMAIN=$1
+    fi
+fi
+
+docker run -d --rm --log-driver=journald \
+    -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
+    -p 32400:32400/tcp
+    -u 1000:1000
+    -v /docker-data/plex:/data
+    --name=plex \
+     plexinc/pms-docker:latest && echo "Roundcube started."
