0ed2c609fff3c5278fd34f17632b099aaf125e8a
Exocen <Exocen@users.noreply.github.com>
2024-04-01 20:31:14 -0600

    derpherp

--- docker_conf/ydl/docker-run.sh
+++ docker_conf/ydl/docker-run.sh
@@ -22,10 +22,13 @@ else
 fi
 
 # Gluetun reset
+curl -X GET --fail --silent --show-error http://localhost:8000/v1/publicip/ip
+
 curl -X PUT --fail --silent --show-error http://localhost:8000/v1/openvpn/status -H "Content-Type: application/json" -d '{"status":"stopped"}'
 # Should get {"outcome":"stopped"}
 curl -X PUT --fail --silent --show-error http://localhost:8000/v1/openvpn/status -H "Content-Type: application/json" -d '{"status":"running"}'
 # Should get{"outcome":"running"}
+curl -X GET --fail --silent --show-error http://localhost:8000/v1/publicip/ips 
 
 docker run \
     --log-driver=journald --rm \
