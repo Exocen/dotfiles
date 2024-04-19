e8a34742d0a810203e6f62950e6aba0e1fd656a9
Exocen <Exocen@users.noreply.github.com>
2024-04-19 00:16:06 -0600

    LAST time, Jonatha, /dev/urandom IS NOT a variable name generator...

--- docker_conf/syncthing/docker-run.sh
+++ docker_conf/syncthing/docker-run.sh
@@ -19,7 +19,7 @@ docker run -d --rm --log-driver=journald \
     -e PUID=1000 \
     -e PGID=1000 \
     -e STDATADIR=/var/syncthing/data \
-    -e STHOMEDIR=/var/syncthing/config \
+    -e STHOMEDIR= \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
     --network=container:gluetun \
     -v /docker-data-nobackup/syncthing/:/var/syncthing/data \
