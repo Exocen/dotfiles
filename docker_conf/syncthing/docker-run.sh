6314db0356b97ee2903397aa4431e0414f95fcde
Exocen <Exocen@users.noreply.github.com>
2024-04-20 21:01:01 -0600

    Please forgive me

--- docker_conf/syncthing/docker-run.sh
+++ docker_conf/syncthing/docker-run.sh
@@ -20,9 +20,9 @@ else
     docker build -t syncthing .
 fi
 
-mkdir -P /docker-data/syncthing/config
+mkdir -p /docker-data/syncthing/config
 chown 1000:1000 -R /docker-data/syncthing/config
-mkdir -P /docker-data-nobackup/syncthing/data
+mkdir -p /docker-data-nobackup/syncthing/data
 chown 1000:1000 -R /docker-data-nobackup/syncthing/data
 
 #PGUID 1000 PGID 1000 -> must have folder permission
