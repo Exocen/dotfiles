8c7ef789523cd57db994d9e37666298323ab419e
Exocen <Exocen@users.noreply.github.com>
2024-05-09 19:12:31 -0600

    Does anyone read this? I'll be at the coffee shop accross the street.

--- docker_conf/nginx_certbot/web_engine.sh
+++ docker_conf/nginx_certbot/web_engine.sh
@@ -3,9 +3,10 @@
 mkdir -p /var/log/nginx /var/log/letsencrypt
 cp -fr /root/fifo-nginx.conf /etc/nginx/nginx.conf
 
-if /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
+if ! /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
+    LOG_DIR=/etc/letsencrypt/logs/
     /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos --logs-dir $LOG_DIR -q -d [DOMAIN] -d git.[DOMAIN] -d mail.[DOMAIN] -d status.[DOMAIN] -d www.[DOMAIN] -d vw.[DOMAIN]
-    if /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
+    if ! /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
         echo "certbot failed exiting..."
         exit 1
     fi
