85434b95586a8c565bcf28a932b6f88c09cc7cce
Exocen <Exocen@users.noreply.github.com>
2024-05-11 03:27:30 -0600

    well crap.

--- docker_conf/nginx_certbot/web_engine.sh
+++ docker_conf/nginx_certbot/web_engine.sh
@@ -16,7 +16,6 @@ fi
 
 openssl dhparam -out /etc/letsencrypt/live/[DOMAIN]/dhparam.pem 4096
 
-
 cp -fr /root/nginx.conf /etc/nginx/nginx.conf
 cp -fr /root/http3.conf /root/http3-main.conf /root/proxy-pass.conf  /etc/nginx/
 pkill 'nginx'
