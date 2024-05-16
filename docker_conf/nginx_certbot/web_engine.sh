d5b8f450eff55447c539096bc258e8a4a05b3b45
Exocen <Exocen@users.noreply.github.com>
2024-05-15 23:41:35 -0600

    Update web_engine.sh

--- docker_conf/nginx_certbot/web_engine.sh
+++ docker_conf/nginx_certbot/web_engine.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 mkdir -p /var/log/nginx /var/log/letsencrypt
-cp -fr /root/fifo-nginx.conf /etc/nginx/nginx.conf
+cp -fr /root/nginx-confs/fifo-nginx.conf /etc/nginx/nginx.conf
 
 if ! /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
     echo "Invalid certificate :"
-    /usr/bin/certbot certificates --max-log-backups 0
+    /usr/bin/certbot certificates
     echo "Creating new certificates :"
-    /usr/bin/certbot --nginx --max-log-backups 0 --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -q -d [DOMAIN] -d git.[DOMAIN] -d mail.[DOMAIN] -d status.[DOMAIN] -d www.[DOMAIN] -d vw.[DOMAIN]
-    if ! /usr/bin/certbot certificates --max-log-backups 0 | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
+    /usr/bin/certbot --nginx --keep-until-expiring --expand --register-unsafely-without-email --agree-tos -q -d [DOMAIN] -d git.[DOMAIN] -d mail.[DOMAIN] -d status.[DOMAIN] -d www.[DOMAIN] -d vw.[DOMAIN]
+    if ! /usr/bin/certbot certificates | grep '[DOMAIN]\|*.[DOMAIN]' &>/dev/null; then
         echo "certbot failed exiting..."
         exit 1
     fi
@@ -16,6 +16,6 @@ fi
 
 [ ! -f /etc/letsencrypt/live/[DOMAIN]/dhparam.pem ] && openssl dhparam -out /etc/letsencrypt/live/[DOMAIN]/dhparam.pem 4096
 
-cp -fr /root/*.conf /etc/nginx/
+cp -fr /root/nginx-confs/*.conf /etc/nginx/
 pkill 'nginx'
 nginx -g 'daemon off;'
