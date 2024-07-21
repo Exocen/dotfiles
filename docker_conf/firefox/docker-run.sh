775e9dc55629daf2c3b068d3dfe402d525c980e3
Exocen <exo@meticae.com>
2024-07-21 06:01:59 -0600

    This is the last time we let loginx commit ascii porn in the comments.

--- docker_conf/firefox/docker-run.sh
+++ docker_conf/firefox/docker-run.sh
@@ -5,14 +5,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-cd "$(dirname "$(readlink -f "$0")")" || exit 1
-
-if docker images | grep "firefox_img" ; then
-    echo "img already created"
-else
-    docker build -t firefox_img .
-fi
-
 #add this cmd to reconnect batch option with /bin/sh interpreter
 # /root/reco.sh
 #PGUID 1000 PGID 1000 -> must have folder permission
@@ -25,4 +17,4 @@ docker run -d --rm --log-driver=journald --log-opt tag="{{.Name}}" \
     -e WEB_LISTENING_PORT=6800 \
     -e VNC_LISTENING_PORT=6900 \
     -v /docker-data-nobackup/firefox/:/config:rw \
-    firefox_img:latest && echo "Firefox started."
+    jlesage/firefox && echo "Firefox started."
