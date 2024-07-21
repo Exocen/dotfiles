36666aa2a190fa5c0894ecab1f15a385215583ef
Exocen <exo@meticae.com>
2024-07-21 06:02:18 -0600

    Same as last commit with changes

--- docker_conf/firefox/reco.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl -X PUT --fail --silent --show-error http://localhost:8000/v1/openvpn/status -H "Content-Type: application/json" -d '{"status":"stopped"}'
-sleep 5s
-curl -X PUT --fail --silent --show-error http://localhost:8000/v1/openvpn/status -H "Content-Type: application/json" -d '{"status":"running"}'
-echo "Ip switch initiated"
-sleep 30s
