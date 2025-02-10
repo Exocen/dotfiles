f87609d75d1cd1cf7eda9d4491e224149190c4a1
Exocen <Exocen@users.noreply.github.com>
2025-02-09 17:33:19 -0700

    woa!! this one was really HARD!

--- bazarr/docker-run.sh
+++ bazarr/docker-run.sh
@@ -4,12 +4,12 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "Must be run as root"
     exit 1
 else
-    if [ -z ${BAZARR_PATH+x} ] ; then
+    if [ -z ${ARR_PATH+x} ] ; then
         if [ -z "$1" ]; then
             echo "No path supplied"
             exit 1
         else
-            BAZARR_PATH=$1
+            ARR_PATH=$1
         fi
     fi
 fi
@@ -21,6 +21,6 @@ docker run -d --rm --log-driver=journald --log-opt tag="{{.Name}}" \
     -e PGID=1000 \
     -e WEBUI_PORTS="6767/tcp,6767/udp" \
     -v /docker-data-nobackup/bazarr:/config \
-    -v "$BAZARR_PATH":/data\
+    -v "$ARR_PATH":/data\
     --name=bazarr \
     ghcr.io/hotio/bazarr && echo "Bazarr started."
