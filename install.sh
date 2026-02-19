191e67bdea27039455cecca73757c3c8035dabe9
Exocen <Exocen@users.noreply.github.com>
2026-02-19 02:41:16 -0700

    added sway as service

--- install.sh
+++ install.sh
@@ -148,6 +148,10 @@ dev_env_install() {
                     # Specific arch .config
                     ln -sfn "$LOCAL/user_conf/zprofile" ~/.zprofile
                     conf_folder user_conf/home_conf
+                    mkdir -p ~/.config/systemd/user/
+                    cp -fr user_conf/systemd_user_services/* ~/.config/systemd/user/
+                    systemctl --user daemon-reload
+                    systemctl --user enable sway.service
                     list=""
                     while IFS= read -r line; do
                         char=$(echo "$line" | head -c1)
@@ -241,18 +245,18 @@ usage() {
 # Options
 while getopts 'hndl:' opt; do
     case $opt in
-    h)
-        usage >&2
-        safeExit true
-        ;;
-    d) debug=true ;;
-    l) logFile="${OPTARG}" ;;
-    n) implied_no=true ;;
-    ?)
-        echo "invalid option: '$1'."
-        usage >&2
-        safeExit true
-        ;;
+        h)
+            usage >&2
+            safeExit true
+            ;;
+        d) debug=true ;;
+        l) logFile="${OPTARG}" ;;
+        n) implied_no=true ;;
+        ?)
+            echo "invalid option: '$1'."
+            usage >&2
+            safeExit true
+            ;;
     esac
 done
 
