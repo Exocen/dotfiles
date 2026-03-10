819bff86c0c793b282666bf78ee29b9c3efc383c
Exocen <Exocen@users.noreply.github.com>
2026-03-09 23:56:56 -0600

    Crap. Tonight is raid night and I am already late.

--- docker_conf/gluetun/docker-run.sh
+++ docker_conf/gluetun/docker-run.sh
@@ -20,7 +20,6 @@ fi
 
 docker run -d --rm --cap-add=NET_ADMIN --name gluetun \
     --log-driver=journald --log-opt tag="{{.Name}}" \
-    -e VPN_SERVICE_PROVIDER=mullvad -e VPN_TYPE=openvpn \
     -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro \
     -p 5800:5800 \
     -p 8384:8384 \
