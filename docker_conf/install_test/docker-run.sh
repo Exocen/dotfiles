65cb23b89aa4228ef64cec518ffcc3e71c6a784a
Exocen <Exocen@users.noreply.github.com>
2026-09-10 05:23:45 -0600

    I dont know what I am doing

--- docker_conf/install_test/docker-run.sh
+++ docker_conf/install_test/docker-run.sh
@@ -47,7 +47,6 @@ function create() {
     docker run \
         --rm -d --name=cont_"$img_name" -v "$logpath":/root/"$img_name" "$img_name"_img 1>/dev/null &&
         echo "$img" started
-    rm -r "$tmpD"
 }
 
 mkdir -p "$dirpath"
